888717c38819d99aafbf21f61e8d20181a9f94f5
이혜천 <rainbow880616@gmail.com>
2022-05-07 22:23:59 +0900

    멀티쓰레드

---
+++ multithread/src/com/hyecheon/locks/reenterantlock/Main.java
@@ -0,0 +1,104 @@
+package com.hyecheon.locks.reenterantlock;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * User: hyecheon lee
+ * Email: rainbow880616@gmail.com
+ * Date: 2022/05/07
+ */
+public class Main {
+    public static class PricesContainer {
+        private Lock lockObject = new ReentrantLock();
+
+        private double bitcoinPrice;
+        private double etherPrice;
+        private double litecoinPrince;
+        private double bitcoinCashPrice;
+        private double ripplePrice;
+
+        public Lock getLockObject() {
+            return lockObject;
+        }
+
+        public void setLockObject(Lock lockObject) {
+            this.lockObject = lockObject;
+        }
+
+        public double getBitcoinPrice() {
+            return bitcoinPrice;
+        }
+
+        public void setBitcoinPrice(double bitcoinPrice) {
+            this.bitcoinPrice = bitcoinPrice;
+        }
+
+        public double getEtherPrice() {
+            return etherPrice;
+        }
+
+        public void setEtherPrice(double etherPrice) {
+            this.etherPrice = etherPrice;
+        }
+
+        public double getLitecoinPrince() {
+            return litecoinPrince;
+        }
+
+        public void setLitecoinPrince(double litecoinPrince) {
+            this.litecoinPrince = litecoinPrince;
+        }
+
+        public double getBitcoinCashPrice() {
+            return bitcoinCashPrice;
+        }
+
+        public void setBitcoinCashPrice(double bitcoinCashPrice) {
+            this.bitcoinCashPrice = bitcoinCashPrice;
+        }
+
+        public double getRipplePrice() {
+            return ripplePrice;
+        }
+
+        public void setRipplePrice(double ripplePrice) {
+            this.ripplePrice = ripplePrice;
+        }
+    }
+
+    private static class PriceUpdater extends Thread {
+        private PricesContainer pricesContainer;
+        private Random random = ThreadLocalRandom.current();
+
+        public PriceUpdater(PricesContainer pricesContainer) {
+            this.pricesContainer = pricesContainer;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                pricesContainer.getLockObject().lock();
+                try {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                    pricesContainer.setBitcoinPrice(random.nextInt(20000));
+                    pricesContainer.setEtherPrice(random.nextInt(2000));
+                    pricesContainer.setLitecoinPrince(random.nextInt(500));
+                    pricesContainer.setBitcoinCashPrice(random.nextInt(5000));
+                    pricesContainer.setRipplePrice(random.nextDouble());
+                } finally {
+                    pricesContainer.getLockObject().unlock();
+                }
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
